e7e2cf35026e2faf17a26d5eb590e22d88dd10cd
Redvus <a.suvorof@gmail.com>
2025-10-31 15:29:48 +0400

    Первые элементы.

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ import "/fonts/fontawesome-free-7.0.0-web/scss/brands.scss";
 // import { Dev } from "./Pages/Dev.js";
 import { Intro } from "./Pages/Intro.js";
 // import { Nav } from "./Components/Nav.js";
-// import { Header } from "./Components/Header.js";
+import { Header } from "./Components/Header.js";
 // import { SidebarRight } from "./Components/SidebarRight.js";
 // import { SidebarLeft } from "./Components/SidebarLeft.js";
 // import { Footer } from "./Components/Footer.js";
